943e13a0f754251e0a5020d5e376800afb8267e4
Jack <jack.dawe@outlook.fr>
2019-10-08 12:24:28 +0200

    Finished reshaping classes to make controller class

--- project/controller.cpp
+++ project/controller.cpp
@@ -4,3 +4,25 @@ Controller::Controller()
 {
 
 }
+
+void State::transition(double a)
+{
+}
+
+double State::reward(double a)
+{
+    return 0;
+}
+
+bool State::isTerminal()
+{
+    return false;
+}
+
+void State::generateStateVector()
+{
+}
+
+vector<int> State::accessibleStates(State s)
+{
+}
--- project/state.cpp
+++ project/state.cpp
@@ -2,28 +2,4 @@
 
 State::State()
 {
-    generateStateVector();
-}
-
-void State::transition(double a)
-{
-}
-
-double State::reward(double a)
-{
-    return 0;
-}
-
-bool State::isTerminal()
-{
-    return false;
-}
-
-void State::generateStateVector()
-{
-    stateVector = stateVector;
-}
-
-vector<int> State::accessibleStates()
-{
 }
