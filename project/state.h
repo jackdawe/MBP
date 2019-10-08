a35e56364d53c727862a1ee1679305ab3e4fc7ae
Jack <jack.dawe@outlook.fr>
2019-10-08 12:22:22 +0200

    Shifted some attributes from state to controller

--- project/state.h
+++ project/state.h
@@ -6,11 +6,7 @@ class State
 {
 public:
     State();
-    virtual void transition(double a); //Changing the state after performing a
-    virtual double reward(double a); //Returns the reward for taking action a
-    virtual bool isTerminal(); //returns true if the state is terminal
-    virtual void generateStateVector(); //Converts any representation of a state to a state vector readable by the agentTrainer
-    virtual vector<int> accessibleStates();
+
 protected:
     vector<double*> stateVector;
 };
