42d133b38ee08336eaf1367ceb84b9bd620efb53
Jack <jack.dawe@outlook.fr>
2019-10-11 14:47:35 +0200

    Implemented MapSS class & code compiles. Need to make an episode player class to debug
    Former-commit-id: fd4952ff486d656c6249066ea66437a3d8be17c1

--- project/Starship/controllerss.cpp
+++ project/Starship/controllerss.cpp
@@ -4,3 +4,38 @@ ControllerSS::ControllerSS()
 {
 
 }
+
+float ControllerSS::transition()
+{
+
+}
+
+bool ControllerSS::isTerminal()
+{
+
+}
+
+void ControllerSS::updateStateVector()
+{
+
+}
+
+int ControllerSS::stateId(State s)
+{
+
+}
+
+void ControllerSS::reset()
+{
+
+}
+
+vector<int> ControllerSS::accessibleStates(State s)
+{
+
+}
+
+int ControllerSS::spaceStateSize()
+{
+
+}
--- project/Starship/waypoint.cpp
+++ project/Starship/waypoint.cpp
@@ -2,5 +2,34 @@
 
 Waypoint::Waypoint()
 {
+}
 
+Vect2d Waypoint::getCentre()
+{
+    return this->centre;
+}
+
+double Waypoint::getRadius()
+{
+    return this->radius;
+}
+
+int Waypoint::getColor()
+{
+    return this->color;
+}
+
+void Waypoint::setCentre(Vect2d u)
+{
+    this->centre = u;
+}
+
+void Waypoint::setRadius(float r)
+{
+    this->radius = r;
+}
+
+void Waypoint::setColor(int color)
+{
+    this->color = color;
 }
