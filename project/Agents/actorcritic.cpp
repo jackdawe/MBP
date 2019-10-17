d8762afe77e3d6feb53508b8df1ceb2f6d192467
Jack <jack.dawe@outlook.fr>
2019-10-17 16:08:10 +0200

    Created ActorCritic agent class
    Former-commit-id: 00b410b2e5c2fe90df219bc2d75ff7fc7584c0cf

---
+++ project/Agents/actorcritic.cpp
@@ -0,0 +1,6 @@
+#include "actorcritic.h"
+
+ActorCritic::ActorCritic()
+{
+
+}
---
+++ project/Agents/actorcritic.h
@@ -0,0 +1,11 @@
+#ifndef ACTORCRITIC_H
+#define ACTORCRITIC_H
+
+
+class ActorCritic
+{
+public:
+    ActorCritic();
+};
+
+#endif // ACTORCRITIC_H
