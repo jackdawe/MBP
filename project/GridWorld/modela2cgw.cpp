8828fa2669e565c16bb7a0335a082b058c16581a
Jack <jack.dawe@outlook.fr>
2019-10-21 15:56:43 +0200

    A2C now runs
    Former-commit-id: 51d116d362d4eddbbfaa986cb8132254309d7793
    Former-commit-id: b7e3e12a2ebd07a10f7ca1fac000e8c1facc2e6f [formerly 7502acbbaaa7a74ee9be17e6ebe3075bb686583e]
    Former-commit-id: 326f86cd73c9027a2bc04fff45c4aa8169825567

--- project/GridWorld/modela2cgw.cpp
+++ project/GridWorld/modela2cgw.cpp
@@ -8,15 +8,15 @@ ModelA2CGW::ModelA2CGW(int nInputs, int nHidden1, int nHidden2, int nHidden3):
 nInputs(nInputs),nHidden1(nHidden1),nHidden2(nHidden2),nHidden3(nHidden3)
 {
     fc1 = register_module("fc1",torch::nn::Linear(nInputs,nHidden1));
-    fc1 = register_module("fc2",torch::nn::Linear(nHidden1,nHidden2));
-    fc1 = register_module("fc3",torch::nn::Linear(nHidden2,nHidden3));
+    fc2 = register_module("fc2",torch::nn::Linear(nHidden1,nHidden2));
+    fc3 = register_module("fc3",torch::nn::Linear(nHidden2,nHidden3));
     actor = register_module("actor",torch::nn::Linear(nHidden3,4));
     critic = register_module("critic",torch::nn::Linear(nHidden3,1));
 }
 
 torch::Tensor ModelA2CGW::forward(torch::Tensor x)
 {
-    x = torch::relu(fc1->forward(x));
+    x = torch::relu(fc1->forward(x.reshape({x.size(0),nInputs})));
     x = torch::relu(fc2->forward(x));
     return torch::relu(fc3->forward(x));
 }
--- project/main.cpp
+++ project/main.cpp
@@ -1,10 +1,9 @@
 #include <QApplication>
-#include "GridWorld/stategw.h"
-#include "actionspace.h"
+//#include "actionspace.h"
 #include "GridWorld/controllergw.h"
-#include "agenttrainer.h"
-#include "Agents/qlearning.h"
-#include "Starship/episodeplayerss.h"
+//#include "agenttrainer.h"
+//#include "Agents/qlearning.h"
+//#include "Starship/episodeplayerss.h"
 #include "Agents/actorcritic.h"
 
 int main(int argc, char *argv[])
@@ -14,9 +13,14 @@ int main(int argc, char *argv[])
     string mapTag = "2_8";
     ControllerGW c(mapTag);
     ModelA2CGW model(c.getSize()*c.getSize()+4,64,128,64);
-    ActorCritic<ControllerGW,ModelA2CGW> agent(model,0.95,0.003,10,20);
+    ActorCritic<ControllerGW,ModelA2CGW> agent(c,model,0.95,0.003,1000,20);
     agent.train();
 
+//    ModelA2CGW model(4,64,128,64);
+//    float a = 2.2;
+//    torch::Tensor x = torch::tensor({a,a,a,a}).view({-1,1});
+//    x = model.actorOutput(x);
+//    cout<<x<<endl;
 //    return a.exec();
 
 }
